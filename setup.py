0156d17e8ed2826d99ef23ce518a817f0616e1a0
Philippe O. Wagner <wagner@arteria.ch>
2014-02-26 22:40:59 +0100

    Changed name for PyPI to python-paymentslip.

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
-setup(name='payment_slip',
+setup(name='python-paymentslip',
       version='0.1.0',
       description='Function for generating ESR-numbers for orange swiss payment slips (so called \"Oranger Einzahlungsschein\").',
       url='http://github.com/philippeowagner/python-payment-slip',
