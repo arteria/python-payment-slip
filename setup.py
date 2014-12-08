149a45dad9dddd90582916278fa31e2be8659696
Philippe O. Wagner <wagner@arteria.ch>
2014-12-08 11:56:49 +0100

    Version bump => 0.1.2

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='python-paymentslip',
-      version='0.1.1',
+      version='0.1.2',
       description='Function for generating ESR-numbers for orange swiss payment slips (so called \"Oranger Einzahlungsschein\").',
       url='http://github.com/philippeowagner/python-payment-slip',
       author='Philippe O. Wagner',
